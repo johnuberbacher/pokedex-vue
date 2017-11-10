0aa093bcc1b9a55c451f53fbb2b9540179eeed8e
John Uberbacher <johnuberbacher@gmail.com>
2017-11-09 23:33:07 -0500

    More progress
    Routing is semi functional

--- js/script.js
+++ js/script.js
@@ -1,35 +1,77 @@
 var dataURL = 'https://pokeapi.co/api/v2/pokemon/';
+const Foo = { 
+	props:  { items : {type: String, required: true} }, 
+	template: '<div v-for="(item, i) in this.items" class="card"><div class="post">Name: {{ this.item.name }}</div><router-link to="/details/{{ vm.item.types }}">Click</router-link></div>'
+}
+const Bar = { 
+	template: '<div>bar</div>' 
+}
+const Details = { 
+	props:  { 
+		itemName : {
+			type: String, 
+			required: true
+		} 
+	},
+	template: '<div>details: {{this.itemName }}{{ self.types }} </div>'
+}
 
-new Vue({
+var vm = new Vue({
   el: '#app',
+  router: new VueRouter({
+    routes: [
+      { 
+        path: '/foo', 
+        props: true,
+        component: Foo 
+	  },
+      { 
+        path: '/bar', 
+        component: Bar 
+      },
+      { 
+		name: 'Details',
+        path: '/details/:itemName', 
+        props: true,
+        component: Details 
+      }
+    ]
+  }),
   data: {
+    loading: null,
     items: null,
-	  title: "Vue Json Iteration",
     showModal: false,
     name: null,
-    items: null,
     sprite: null,
     types: null,
     weight: null,
     abilities: null
   },
   created: function () {
+    this.loading = true;
     this.fetchData();
+  },
+  watch: function () {
+	this.fetchData();
   },
   methods: {
     fetchData: function () {
   		var self = this;
-  		$.get(dataURL, function( data ) {
+  		$.get(dataURL + '?limit=151&offset=0', function( data ) {
   			self.items = data.results;
+			self.loading = false;
   		});
     },
-    getDetails: function (unique) {
-      var detailsURL = dataURL + unique;
+    getDetails: function (unique,event) {
+	  var self = this;
+	  self.loading = true;
+      var detailsURL = dataURL + unique.name;
       $.get(detailsURL, function( details ) {
-        self.name = details.name;
+		self.name = details.name;
         self.types = details.types;
-        console.log(details.name);
-        console.log(details.types);
+		console.log(details.name);
+		console.log(details.types);
+		self.loading = false;
       });
     }
   }
